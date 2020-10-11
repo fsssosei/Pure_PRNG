a4701346d47fbab212521d640955ee90b4cb3eec
fsssosei <43291778+fsssosei@users.noreply.github.com>
2020-10-11 23:02:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 '''
-pseudo_random_number_generator - This is the package used to generate pseudo-random Numbers.
+pseudo_random_number_generator - This package is used to generate multi-precision pseudo-random Numbers.
 Copyright (C) 2020  sosei
 
 This program is free software: you can redistribute it and/or modify
@@ -23,15 +23,15 @@ with open("README.md", encoding='utf-8') as fh:
 
 setup(
     name='pseudo_random_number_generator',
-    version='0.7.0',
-    description='Generate pseudo-random number package in python.',
+    version='0.7.1',
+    description='Generate multi-precision pseudo-random number package in python.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/fsssosei/pseudo_random_number_generator',
     license='GNU Affero General Public License v3',
     author='sosei',
     author_email='fss.sosei@gmail.com',
-    keywords=['pseudo', 'random', 'number'],
+    keywords=['PRN', 'Random', 'Multiple-precision'],
     python_requires='>=3.7',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
