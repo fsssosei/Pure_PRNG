dab144f364cdfbc18ca4e0b806a398c96dfcfa09
fsssosei <43291778+fsssosei@users.noreply.github.com>
2020-10-12 17:15:16 +0800

    Update pseudo_random_number_generator_module.py

--- src/pseudo_random_number_generator_package/pseudo_random_number_generator_module.py
+++ src/pseudo_random_number_generator_package/pseudo_random_number_generator_module.py
@@ -146,9 +146,9 @@ class prng_class(object):
             --------
             >>> prng_instance = prng_class(170141183460469231731687303715884105727)
             >>> prng_instance.rand_float()
-            0.6326937641706669741872583730940429737405414921354622618051716414693676562568173
+            mpfr('0.6326937641706669741872583730940429737405414921354622618051716414693676562568173',257)
             >>> prng_instance.rand_float(115792089237316195423570985008687907853269984665640564039457584007913129639747)
-            0.3513956730047900426657412504296283135563682862601256213620310087617832883445811
+            mpfr('0.02795744845257346733436109648463446736744766610965612207643215290679786849301309',257)
         '''
         prng_period = self.__class__.prng_period_dict[self.prng_type]
         bit_num_of_prng_period = prng_period.bit_length() + ((prng_period & 1) - 1)
@@ -179,7 +179,7 @@ class prng_class(object):
             >>> prng_instance.rand_int(100, 1)
             64
             >>> prng_instance.rand_int(100, 1, 115792089237316195423570985008687907853269984665640564039457584007913129639747)
-            36
+            3
         '''
         assert isinstance(b, int), 'Error: The value of b is non-integer.'
         assert isinstance(a, int), 'Error: The value of a is non-integer.'
@@ -239,7 +239,7 @@ class prng_class(object):
             --------
             >>> prng_instance = prng_class(170141183460469231731687303715884105727)
             >>> prng_instance.random_integer_number_with_definite_period(115792089237316195423570985008687907853269984665640564039457584007913129639747)
-            73260932800743358445652462028207907455677987852735468159219395093090100006110
+            40688839126177430252467309162469901643963863918059158449302074429100738061375
         '''
         assert isinstance(new_period, int), 'Error: The value of new_period is non-integer.'
         prng_period = self.__class__.prng_period_dict[self.prng_type]
