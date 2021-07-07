03ca36af8e155850e201838c1a4a21a2fc663436
fsssosei <43291778+fsssosei@users.noreply.github.com>
2021-07-08 01:25:39 +0800

    Update __init__.py

--- src/prng_algorithms_package/__init__.py
+++ src/prng_algorithms_package/__init__.py
@@ -19,4 +19,5 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from prng_algorithms_package.ICG_module import *
 from prng_algorithms_package.LCG64_32_ext_module import *
 from prng_algorithms_package.XSM64_module import *
+from prng_algorithms_package.SplitMix64_module import *
 from prng_algorithms_package.SquaresCounter_module import *
