d51c4a8caa17c1b4ba3a6d9f3b04e24e9a06078b
fsssosei <43291778+fsssosei@users.noreply.github.com>
2021-07-10 06:49:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", encoding='utf-8') as fh:
 
 setup(
     name='pure_prng',
-    version='2.8.0',
+    version='2.8.1',
     description='Generate professional pseudo-random number package in python.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -41,5 +41,5 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages('src'),
     include_package_data=True,
-    install_requires=['gmpy2>=2.0.8', 'randomgen>=1.19.3', 'pure-nrng>=1.1.0']
+    install_requires=['gmpy2>=2.0.8', 'randomgen>=1.20.3', 'pure-nrng>=1.1.0']
 )
